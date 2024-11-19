f34358f3f73efba1c4186ee3c9d17a98b940f687
WONGTO6 <wongto6@oocl.com>
2024-11-19 16:28:35 +0800

    test: test return after right turn status when right turn with dir S

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -106,6 +106,16 @@ public class MarsRoverTest {
         Assertions.assertEquals(expected_output, finalPosDir);
     }
 
+    @Test
+    void should_return_after_right_turn_status_when_right_turn_W() {
+        String expected_output = "0:0:N";
+        MarsRover marsRover = new MarsRover(0, 0, "W");
+        String finalPosDir = marsRover.controlMarsRover("R");
+        Assertions.assertEquals(expected_output, finalPosDir);
+    }
+
+
+
 
 
 
