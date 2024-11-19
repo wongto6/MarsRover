d341e20177ae11aef15987329ddc443118bc7972
WONGTO6 <wongto6@oocl.com>
2024-11-19 17:20:34 +0800

    refactor: use enum obj length

--- src/main/java/Direction.java
+++ src/main/java/Direction.java
@@ -2,10 +2,10 @@ public enum Direction {
     N, E, S, W;
 
     public Direction turnLeft() {
-        return Direction.values()[(this.ordinal() + 3) % 4];
+        return Direction.values()[(this.ordinal() + (Direction.values().length - 1)) % Direction.values().length];
     }
 
     public Direction turnRight() {
-        return Direction.values()[(this.ordinal() + 1) % 4];
+        return Direction.values()[(this.ordinal() + 1) % Direction.values().length];
     }
 }
